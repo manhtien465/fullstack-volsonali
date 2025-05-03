80d7c84d5daa34a967f41702d79e520a60fc12cd
tien.nguyen <nguyenmtien465@gmail.com>
2025-05-04 00:29:02 +0700

    fix: fix build

---
+++ server/src/api/template-json-page/controllers/template-json-page.ts
@@ -0,0 +1,7 @@
+/**
+ * template-json-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::template-json-page.template-json-page');
---
+++ server/src/api/template-json-page/routes/template-json-page.ts
@@ -0,0 +1,7 @@
+/**
+ * template-json-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::template-json-page.template-json-page');
