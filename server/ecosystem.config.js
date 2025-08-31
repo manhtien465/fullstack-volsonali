573539d8b72f70e22905df76c64ee59c6f044afa
manhtien465 <tien.nguyen@savvycomsoftware.com>
2025-09-01 00:47:47 +0700

    fix: fix pm2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
         script: 'npm', // Use npm to run the start script
         args: 'start', // Runs `npm start` (for production)
         cwd: './', // Current working directory (root of your Strapi project)
-        instances: 1, // Number of instances (use 'max' for cluster mode, but Strapi may need single instance)
-        exec_mode: 'fork', // Fork mode is typically used for Strapi (cluster mode may cause port conflicts)
+        instances: 2, // Number of instances (use 'max' for cluster mode, but Strapi may need single instance)
+        exec_mode: 'cluster', // Fork mode is typically used for Strapi (cluster mode may cause port conflicts)
         autorestart: true, // Restart app if it crashes
         watch: false, // Set to true in development to restart on file changes
         max_memory_restart: '1G', // Restart if memory usage exceeds 1GB
