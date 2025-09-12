704b7b67978c3f795a9c0b8c076421acd221a98b
manhtien465 <nguyenmtien465@gmail.com>
2025-09-12 09:44:30 +0700

    fix pm2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     apps: [
       {
-        name: 'strapi-app', // Name of your application in PM2
+        name: 'admin-megagamefun.net', // Name of your application in PM2
         script: 'npm', // Use npm to run the start script
         args: 'start', // Runs `npm start` (for production)
         cwd: './', // Current working directory (root of your Strapi project)
-        instances: 2, // Number of instances (use 'max' for cluster mode, but Strapi may need single instance)
+        instances: 1, // Number of instances (use 'max' for cluster mode, but Strapi may need single instance)
         exec_mode: 'cluster', // Fork mode is typically used for Strapi (cluster mode may cause port conflicts)
         autorestart: true, // Restart app if it crashes
         watch: false, // Set to true in development to restart on file changes
