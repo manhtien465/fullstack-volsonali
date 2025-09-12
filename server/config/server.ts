a4672614cc1931255ee18cdfa3c86bd1f17d8ed7
manhtien465 <nguyenmtien465@gmail.com>
2025-09-12 17:09:21 +0700

    fix config

--- server/config/server.ts
+++ server/config/server.ts
@@ -3,7 +3,7 @@ import cronTasks from "./cron-tasks";
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://admin.wikixp.org'),
+  url: env('URL', 'https://admin.megagamefun.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
