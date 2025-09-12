585fd6cead3e2f94b52948520a947add0240162c
manhtien465 <nguyenmtien465@gmail.com>
2025-09-12 09:49:18 +0700

    fix: Fix api

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
         args: 'start', // Runs `npm start` (for production)
         cwd: './', // Current working directory (root of your Strapi project)
         instances: 1, // Number of instances (use 'max' for cluster mode, but Strapi may need single instance)
-        exec_mode: 'cluster', // Fork mode is typically used for Strapi (cluster mode may cause port conflicts)
+        exec_mode: 'fork', // Fork mode is typically used for Strapi (cluster mode may cause port conflicts)
         autorestart: true, // Restart app if it crashes
         watch: false, // Set to true in development to restart on file changes
         max_memory_restart: '1G', // Restart if memory usage exceeds 1GB
